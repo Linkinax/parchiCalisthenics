b959063849c8022a74d737ba319d2f394b8f21d4
Alex Jitariu <coonax@gmail.com>
2021-12-31 16:17:55 +0100

    Moving data from being static to being passed to the controller and displayed within the view

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,37 @@
 
 //Get home page
 const homeList= (req, res) =>{
-    res.render('location-list', {title: "Home"} );
+    res.render('location-list', {
+        title: "Trova il parco più vicino a te!",
+        pageHeader: {
+            title: 'Parchi Calisthenics',
+            strapline: 'Trova il parco più vicino a te'
+        },
+
+        locations: [
+            {
+                name: 'Parco della Questura',
+                address: 'Via della questura 123, Brescia',
+                rating:3,
+                facilities: ['Sbarra trazioni ', 'Parrallele ', 'Panca abs '],
+                distance: '100m'
+            },
+            {
+                name: 'Campo Marte',
+                address: 'Via campo marte',
+                rating:4,
+                facilities: ['Sbarra trazioni ', 'Parrallele ', 'Panca abs '],
+                distance: "1200m"
+            },
+            {
+                name: 'Parco xyz',
+                address: 'Via della xyz 123, Brescia',
+                rating:2,
+                facilities: ["Ai", "B", "C"],
+                distance: "9999m"
+            }
+        ]
+    } );
 
 }
 
@@ -13,9 +43,10 @@ const locationInfo= (req, res) =>{
 
 //Get AddReview
 const addReview= (req, res) =>{
-    res.render('index', {title: "Add review"} );
-
+    res.render('location-review-form', {title: "Add review"} );
 }
+
+
 
 module.exports = {
     homeList,
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,8 +1,7 @@
 
 //Get about page
 const about= (req, res) =>{
-    res.render('index', {title: "About"} );
-
+    res.render('generic-text', {title: "About"} );
 }
 
 module.exports = {

9af4caa487d03ce7c91ce5926cfb319ce15a7184
Alex Jitariu <coonax@gmail.com>
2022-01-15 15:23:16 +0100

    Changing Mongo DB username & password & hiding api

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,13 @@
 const mongoose= require('mongoose');
 require('./locations');
 
+
 const dbURI = "mongodb+srv://link:V3l3n0@cluster0.zbdfn.mongodb.net/TestParchiCalisthenics";
+
+const secretDBUri = process.env.MONGODB_CONNECTION_URI;
+
+console.log("Here's the secret: "+ secretDBUri);
+
 mongoose.connect(dbURI, {useNewURLparser:true});
 
 
